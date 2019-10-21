1d57275b21f6bd9065345b2d2783479ef252fbf6
Fredrik Ås <fredaas86@gmail.com>
2019-10-21 11:59:56 +0200

    Make configure script more intuitive to use

--- sdl-rps/configure.sh
+++ sdl-rps/configure.sh
@@ -1,48 +1,67 @@
 #!/usr/bin/env bash
-#
-# SYNTAX
-#
-#   source configure.sh [options]
-#
-# OPTIONS
-#
-#   -c
-#
-#     Combination of initialization and cell selection.
-#
+
+read -d '' usage << EOF
+USAGE
+
+    . configure <options>
+
+OPTIONS
+
+    -c
+
+        Select perturbation mode and neighbor selection:
+
+            1. '3', 'all'
+
+            2. '3', 'diagonal'
+
+            3. 'random', 'all'
+
+            4. 'random', diagonal'
+
+    -f
+
+        Set fullscreen mode.
+EOF
 
 if [[ $# -eq 0 ]]; then
-    echo "[INFO] Using default settings"
-    export CFLAGS=" -DRPS_INIT_TRI -DRPS_CELL_ALL"
-    return 0
+    IFS=$'\n'
+    echo "${usage[*]}"
 fi
 
-while [[ $# -gt 0 ]];
-do
+CFLAGS=""
+while [[ $# -gt 0 ]]; do
     case "$1" in
     "-c")
         case "$2" in
-        "tri-all")
-            export CFLAGS=" -DRPS_INIT_TRI -DRPS_CELL_ALL"
+        "1")
+            CFLAGS+=" -DRPS_INIT_TRI -DRPS_CELL_ALL"
             ;;
-        "tri-diag")
-            export CFLAGS=" -DRPS_INIT_TRI -DRPS_CELL_DIAG"
+        "2")
+            CFLAGS+=" -DRPS_INIT_TRI -DRPS_CELL_DIAG"
             ;;
-        "rand-all")
-            export CFLAGS=" -DRPS_INIT_RAND -DRPS_CELL_ALL"
+        "3")
+            CFLAGS+=" -DRPS_INIT_RAND -DRPS_CELL_ALL"
             ;;
-        "rand-diag")
-            export CFLAGS=" -DRPS_INIT_RAND -DRPS_CELL_DIAG"
+        "4")
+            CFLAGS+=" -DRPS_INIT_RAND -DRPS_CELL_DIAG"
             ;;
         *)
-            echo "[ERROR] Invalid option"
-            break
+            echo "[ERROR] Invalid option..."
+            break;
         esac
         shift
         shift
         ;;
+    "-f")
+        CFLAGS+=" -DRPS_FULLSCREEN"
+        shift
+        ;;
     *)
-        echo "[ERROR] Invalid option"
-        break;
+        echo "[ERROR] Invalid option..."
+        break
     esac
 done
+
+export CFLAGS
+export OMP_NUM_THREADS=8
